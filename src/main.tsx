58186528c1626f17c8e7cd8a245c2eb759baae79
Mohannad <magad30011@gmail.com>
2026-04-29 23:34:39 -0700

    update admin and fixes2

--- src/main.tsx
+++ src/main.tsx
@@ -2,25 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
-import { useAuth } from "./lib/auth"; // ✅ مهم
+import { AuthProvider } from "./lib/auth"; // 🔥 مهم جدًا
 import "./styles.css";
-
-// const router = getRouter();
-
-// ✅ نحطها داخل component عشان نستخدم useAuth
-function App() {
-  const { user } = useAuth();
-
-  return (
-    <RouterProvider
-      router={router}
-      context={{ user }} // 🔥 هذا هو الحل
-    />
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider> {/* 🔥 يحل مشكلة الشاشة البيضاء */}
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
